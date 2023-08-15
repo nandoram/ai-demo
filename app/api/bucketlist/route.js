9ea596b3be8397560e1135a13323725e3f88d148
Nando R <nando.rambiriche@gmail.com>
2023-08-15 15:58:27 -0400

    test build

--- app/api/bucketlist/route.js
+++ app/api/bucketlist/route.js
@@ -18,7 +18,7 @@ export async function GET(request, response) {
     model: "gpt-3.5-turbo",
     messages: [{"role": "system", "content": "You will generate a long bucketlist based on answers given from a personality quiz."}, {role: "user", content: `Generate a personalized and specific bucketlist based on the following questions and answers: Q ${questionArray[0]} A. ${answers.adventure}, Q ${questionArray[1]} A. ${answers.social}, Q ${questionArray[2]} A. ${answers.nature}, Q ${questionArray[3]} A. ${answers.creativity}, Q ${questionArray[4]} A. ${answers.cultural}, Q ${questionArray[5]} A. ${answers.learning}, Q ${questionArray[6]} A. ${answers.physical}, Q ${questionArray[7]} A. ${answers.food}, Q ${questionArray[8]} A. ${answers.historical}, Q ${questionArray[9]} A. ${answers.family}`}],
   });
- let generatedBucketList = completion.data.choices[0].message.content
+  let generatedBucketList = completion.data.choices[0].message.content
   generatedBucketList =generatedBucketList.split(/[0-9]+\./)
   console.log(generatedBucketList)
   return NextResponse.json({ generatedBucketList })
