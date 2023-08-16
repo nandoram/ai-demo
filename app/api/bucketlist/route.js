9ef85533aa864086c60bc7bfe9c4f72e1220d98b
Nando R <nando.rambiriche@gmail.com>
2023-08-15 20:47:16 -0400

    optimized prompt

--- app/api/bucketlist/route.js
+++ app/api/bucketlist/route.js
@@ -4,7 +4,6 @@ import { questions } from '@/app/demos/bucketlist/questionsData';
 export async function GET(request, response) {
   const { searchParams } = new URL(request.url);
   const answers = JSON.parse(searchParams.get('answers'));
-  console.log("function called");
   let questionArray = [];
   questions.map((data, i) => { 
     questionArray.push(data.content)
@@ -16,10 +15,9 @@ export async function GET(request, response) {
   const openai = new OpenAIApi(configuration);
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
-    messages: [{"role": "system", "content": "You will generate a long bucketlist based on answers given from a personality quiz."}, {role: "user", content: `Generate a personalized and specific bucketlist based on the following questions and answers: Q ${questionArray[0]} A. ${answers.adventure}, Q ${questionArray[1]} A. ${answers.social}, Q ${questionArray[2]} A. ${answers.nature}, Q ${questionArray[3]} A. ${answers.creativity}, Q ${questionArray[4]} A. ${answers.cultural}, Q ${questionArray[5]} A. ${answers.learning}, Q ${questionArray[6]} A. ${answers.physical}, Q ${questionArray[7]} A. ${answers.food}, Q ${questionArray[8]} A. ${answers.historical}, Q ${questionArray[9]} A. ${answers.family}`}],
+    messages: [{role: "user", content: `Generate a long, specific and personalized bucketlist based on the following questions and answers: Q ${questionArray[0]} A. ${answers.adventure}, Q ${questionArray[1]} A. ${answers.social}, Q ${questionArray[2]} A. ${answers.nature}, Q ${questionArray[3]} A. ${answers.creativity}, Q ${questionArray[4]} A. ${answers.cultural}, Q ${questionArray[5]} A. ${answers.learning}, Q ${questionArray[6]} A. ${answers.physical}, Q ${questionArray[7]} A. ${answers.food}, Q ${questionArray[8]} A. ${answers.historical}, Q ${questionArray[9]} A. ${answers.family}`}],
   });
   let generatedBucketList = completion.data.choices[0].message.content
   generatedBucketList =generatedBucketList.split(/[0-9]+\./)
-  console.log(generatedBucketList)
   return NextResponse.json({ generatedBucketList })
 }
