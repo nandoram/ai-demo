277f39de26459d8f72bfad1fbc552ca14c3ab839
Nando R <nando.rambiriche@gmail.com>
2023-08-18 15:47:29 -0400

    updated error

--- app/api/bucketlist/route.js
+++ app/api/bucketlist/route.js
@@ -3,14 +3,14 @@ import { NextResponse } from 'next/server'
 export async function GET(request, response) {
   const { searchParams } = new URL(request.url);
   const answers = JSON.parse(searchParams.get('answers'));
-  let content;
+  let rawResponse;
   try {
-    const rawResponse = await fetch('https://dpojactvu7.execute-api.us-east-1.amazonaws.com/createBucketList', {
+    rawResponse = await fetch('https://dpojactvu7.execute-api.us-east-1.amazonaws.com/createBucketList', {
       method: 'POST',
       body: JSON.stringify({ content: `Generate a specific and personalized bucketlist based on someone that rates adventure seeking a ${answers.adventure} out of 10, social interactions a ${answers.social} out of 10, nature and outdoors a ${answers.nature} out of 10, creativity a ${answers.creativity} out of 10, cultural exploration a ${answers.cultural} out of 10, learning and growth a ${answers.learning} out of 10, physical activity a ${answers.physical} out of 10, food and culinary experiences a ${answers.food} out of 10, historical and cultural sites a ${answers.historical} out of 10, family and friends a ${answers.family} out of 10`}),
     });
-    content = await rawResponse.json();
-    if (content){
+    if (rawResponse){
+      const content = await rawResponse.json();
       const generatedBucketList = content.message
   
       await fetch('https://dpojactvu7.execute-api.us-east-1.amazonaws.com/addBucketList', {
