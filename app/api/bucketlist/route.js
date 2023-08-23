b9edc3b361e1c2ba3f31c99298d55ee90ca20ea3
Nando R <nando.rambiriche@gmail.com>
2023-08-23 15:29:47 -0400

    removed try catch

--- app/api/bucketlist/route.js
+++ app/api/bucketlist/route.js
@@ -5,16 +5,11 @@ export async function GET(request, response) {
   const answers = JSON.parse(searchParams.get('answers'));
   const questionsAndAnswers = {adventure: answers.adventure, social: answers.social, nature: answers.nature, creativity: answers.creativity, cultural: answers.cultural, learning: answers.learning, physical: answers.physical, food: answers.food, historical: answers.historical, family: answers.family}
   let rawResponse;
-  try {
     rawResponse = await fetch('https://dpojactvu7.execute-api.us-east-1.amazonaws.com/createBucketList', {
       method: 'POST',
       body: JSON.stringify({ content: `Generate a specific and personalized bucketlist based on someone that rates adventure seeking a ${answers.adventure} out of 10, social interactions a ${answers.social} out of 10, nature and outdoors a ${answers.nature} out of 10, creativity a ${answers.creativity} out of 10, cultural exploration a ${answers.cultural} out of 10, learning and growth a ${answers.learning} out of 10, physical activity a ${answers.physical} out of 10, food and culinary experiences a ${answers.food} out of 10, historical and cultural sites a ${answers.historical} out of 10, family and friends a ${answers.family} out of 10`}),
     });
 
-  }
-  catch (err) {
-    return NextResponse.json({ error: 'timed out' })
-  }
   if (rawResponse){
     const content = await rawResponse.json();
     const generatedBucketList = content.message
